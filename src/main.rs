6f771c45e990d4320363a48fdef312a8e65844d1
Rob Pellegrin <>
2025-10-24 22:42:20 -0400

    Added function to create symlink

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 mod args;
-use args::Args;
-use clap::Parser;
-
-// Interm program name.
-const PROGRAM_NAME: &str = "rstow";
+use std::os::unix::fs::symlink;
+use std::path::Path;
 
 fn main() {
-    let args = Args::parse();
-    println!("Welcome to {}", PROGRAM_NAME);
+    create_link().expect("Failed to create symlink");
+}
+
+fn create_link() -> std::io::Result<()> {
+    let target = Path::new("target_file.txt");
+    let link = Path::new("my_symlink.txt");
+
+    symlink(target, link)?;
+    println!(
+        "Symlink created: {} -> {}",
+        link.display(),
+        target.display()
+    );
+
+    Ok(())
 }
